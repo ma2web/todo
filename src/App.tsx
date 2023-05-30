7f0ce903e50c3903f4e49786dce41e214003f033
Amin <mohammadazimi16@gmail.com>
2023-05-31 02:56:47 +0300

    initial commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EditTask from './pages/EditTask/EditTask';
+import Tasks from './pages/Tasks/Tasks';
+import { StoreProvider } from './store/Store';
+import theme from './theme/theme';
 
-function App() {
+type Props = {};
+
+const App = (props: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <ThemeProvider theme={theme}>
+        <div className='App'>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Tasks />} />
+              <Route path='/task/:id' element={<EditTask />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </StoreProvider>
   );
-}
+};
 
 export default App;
